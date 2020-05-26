7f995f2f04d4e33c125c40bb3f59fbd482a8c0ea
Mulenga Bowa <mulengabowa53@gmail.com>
2020-05-26 02:11:07 +0200

    initial commit

---
+++ lib/activestorage/ipfs.rb
@@ -0,0 +1,8 @@
+require "activestorage/ipfs/version"
+
+module Activestorage
+  module Ipfs
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/activestorage/ipfs/version.rb
@@ -0,0 +1,5 @@
+module Activestorage
+  module Ipfs
+    VERSION = "0.1.0"
+  end
+end
