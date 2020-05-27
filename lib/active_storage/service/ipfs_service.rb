79985b86092dfd0762b36030a2b1968cf1883016
Mulenga Bowa <mulengabowa53@gmail.com>
2020-05-28 00:22:27 +0200

    added download_chunks and download methods

--- lib/active_storage/service/ipfs_service.rb
+++ lib/active_storage/service/ipfs_service.rb
@@ -1,15 +1,34 @@
+require 'ipfs/client'
 module ActiveStorage
   class Service::IpfsService < Service
     attr_reader :client
 
-    def initialize(endpoint:)
-      @client = Ipfs::Client.new(endpoint)
+    def initialize(api_endpoint:, gateway_endpoint:)
+      @client = Ipfs::Client.new(api_endpoint, gateway_endpoint)
     end
 
+    # File is uploaded to Ipfs and a hash
+    # is returned which is used to retrieve the file
+    # Change the key of the blob to that of the hash
     def upload(key, io, checksum: nil, **)
       instrument :upload, key: key, checksum: checksum do
-        @client.add(io.path)
+        data = @client.add(io.path)
+        Blob.find_by_key(key).update(key: data['Hash'])
       end
+    end
+
+    def download(key, &block)
+      if block_given?
+        @client.download_chunks key, &block
+      else
+        instrument :download, key: key do
+          @client.download(key)
+        end
+      end
+    end
+
+    def url(key, content_type: nil, filename: nil, expires_in: nil, disposition: nil)
+      @client.build_file_url(key)
     end
   end
 end
--- lib/ipfs/client.rb
+++ lib/ipfs/client.rb
@@ -7,30 +7,43 @@ module Ipfs
   class Error < StandardError; end
 
   class Client
-    attr_reader :endpoint, :http_client
+    attr_reader :api_endpoint, :gateway_endpoint, :http_client
 
-    def initialize(endpoint)
-      @endpoint = endpoint
+    def initialize(api_endpoint, gateway_endpoint)
+      @api_endpoint = api_endpoint
+      @gateway_endpoint = gateway_endpoint
       @http_client = HTTP
     end
 
     def add(path)
-      response = @http_client.post(
-        "#{@endpoint}/api/v0/add",
+      res = @http_client.post(
+        "#{@api_endpoint}/api/v0/add",
         form: {
           file: HTTP::FormData::File.new(path)
         }
       )
 
-      if response.code >= 200 && response.code <= 299
-        JSON.parse(response.body)
+      if res.code >= 200 && res.code <= 299
+        JSON.parse(res.body)
       else
-        raise Error, response.body
+        raise Error, res.body
       end
     end
 
+    def download_chunks(hash, &block)
+      url = build_file_url(hash)
+      res = @http_client.get(url)
+      res.body.each(&block)
+    end
+
+    def download(hash)
+      url = build_file_url(hash)
+      res = @http_client.get(url)
+      res.body
+    end
+
     def build_file_url(hash)
-      "#{@endpoint}/ipfs/#{hash}"
+      "#{@gateway_endpoint}/ipfs/#{hash}"
     end
   end
 end
