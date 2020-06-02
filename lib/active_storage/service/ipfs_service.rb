ffb8acea2f3db9884bd5c9af847fbf16fb35c4d3
Mulenga Bowa <mulengabowa53@gmail.com>
2020-06-02 18:02:22 +0200

    finished server upload

--- lib/active_storage/service/ipfs_service.rb
+++ lib/active_storage/service/ipfs_service.rb
@@ -4,7 +4,7 @@ module ActiveStorage
     attr_reader :client
 
     def initialize(api_endpoint:, gateway_endpoint:)
-      @client = Ipfs::Client.new(api_endpoint, gateway_endpoint)
+      @client = Ipfs::Client.new api_endpoint, gateway_endpoint
     end
 
     # File is uploaded to Ipfs and a hash
@@ -12,23 +12,55 @@ module ActiveStorage
     # Change the key of the blob to that of the hash
     def upload(key, io, checksum: nil, **)
       instrument :upload, key: key, checksum: checksum do
-        data = @client.add(io.path)
-        Blob.find_by_key(key).update(key: data['Hash'])
+        data = @client.add io.path
+        find_blob_by_key(key).update(key: data['Hash'])
+        # TODO: Ensure integrity of checksum
       end
     end
 
     def download(key, &block)
       if block_given?
-        @client.download_chunks key, &block
+        instrument :streaming_download, key: key do
+          @client.download key, &block
+        end
       else
         instrument :download, key: key do
-          @client.download(key)
+          # Make a query to check if file exists before
+          # making download request to prevent hanging
+          # requests when file doesn't exist
+          @client.file_exists! key
+          @client.download key
+        rescue Ipfs::NotFoundError
+          raise ActiveStorage::FileNotFoundError
         end
       end
     end
 
+    def download_chunk(key, range)
+      instrument :download_chunk, key: key, range: range do
+        @client.file_exists! key
+        @client.cat key, range.begin, range.size
+      rescue Ipfs::NotFoundError
+        raise ActiveStorage::FileNotFoundError
+      end
+    end
+
     def url(key, content_type: nil, filename: nil, expires_in: nil, disposition: nil)
-      @client.build_file_url(key)
+      @client.build_file_url key, filename.to_s
+    end
+
+    def exists?(key)
+      instrument :exist, key: key do |payload|
+        answer = @client.file_exists?(key)
+        payload[:exist] = answer
+        answer
+      end
+    end
+
+    private
+
+    def find_blob_by_key(key)
+      Blob.find_by_key key
     end
   end
 end
--- lib/ipfs/client.rb
+++ lib/ipfs/client.rb
@@ -5,6 +5,7 @@ require 'json'
 
 module Ipfs
   class Error < StandardError; end
+  class NotFoundError < Error; end
 
   class Client
     attr_reader :api_endpoint, :gateway_endpoint, :http_client
@@ -30,20 +31,46 @@ module Ipfs
       end
     end
 
-    def download_chunks(hash, &block)
-      url = build_file_url(hash)
-      res = @http_client.get(url)
-      res.body.each(&block)
-    end
-
-    def download(hash)
-      url = build_file_url(hash)
-      res = @http_client.get(url)
+    def cat(hash, offset, length)
+      res = @http_client.get("#{@api_endpoint}/api/v0/cat?arg#{hash}&offset=#{offset}&length=#{length}")
       res.body
     end
 
-    def build_file_url(hash)
-      "#{@gateway_endpoint}/ipfs/#{hash}"
+    def dht_findProvs(hash, &block)
+      res = @http_client.post("#{@api_endpoint}/api/v0/dht/findprovs?arg=#{hash}")
+      res.body.each(&block)
+    end
+
+    def download(hash, &block)
+      url = build_file_url(hash)
+      res = @http_client.get(url)
+
+      if block_given?
+        res.body.each(&block)
+      else
+        res.body
+      end
+    end
+
+    def file_exists?(key)
+      dht_findProvs(key) do |chunk|
+        res = chunk.split("\n")[0]
+        res_json = JSON.parse res
+        return res_json['Type'] == 4
+      end
+    end
+
+    def file_exists!(key)
+      dht_findProvs(key) do |chunk|
+        res = chunk.split("\n")[0]
+        res_json = JSON.parse res
+        raise NotFoundError, key unless res_json['Type'] == 4
+      end
+    end
+
+    def build_file_url(hash, filename = '')
+      query = filename.empty? ? '' : "?filename=#{filename}"
+      "#{@gateway_endpoint}/ipfs/#{hash}#{query}"
     end
   end
 end
